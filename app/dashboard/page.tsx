29f329945e2ae7a3aa8e7caa97dae54b17dcbaf7
Azekowka <gabjtov@gmail.com>
2025-04-27 16:22:15 +0500

    dashboard cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import Layout from "../../components/dashboard/layout"
+import CardDemo1 from "@/components/cards-demo-1"
+import CardDemo2 from "@/components/cards-demo-2"
+import CardDemo3 from "@/components/cards-demo-3"
 
 export default function Dashboard() {
   return (
     <div data-theme="dark">
       <Layout>
-        <div>Dashboard Content</div>
+        <div className="flex flex-row justify-center items-stretch gap-6 mx-auto max-w-6xl mt-20">
+          <div className="flex-1 w-full max-w-sm min-h-[24rem]">
+            <CardDemo2 />
+          </div>
+          <div className="flex-1 w-full max-w-sm min-h-[24rem]">
+            <CardDemo3 />
+          </div>
+          <div className="flex-1 w-full max-w-sm min-h-[24rem]">
+            <CardDemo1 />
+          </div>
+        </div>
       </Layout>
     </div>
   )
